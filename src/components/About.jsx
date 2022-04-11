f6e4f5f7b1280f3992f3d7136fb2e88e8602170a
unknown <steinway.nathan@gmail.com>
2022-04-10 23:42:47 -0500

    completed skeleton for nav and About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,12 +7,22 @@ function About() {
 
         <div className='mx-auto px-8 flex flex-col justify-center h-full'>
 
-            <div className='text-center'>
+            <div className='text-center pt-5'>
                 <h1 className='text-5xl'>NATHAN STEINWAY</h1>
             </div>
+
+            <dive className='text-center pt-5'>
+                <p>This is just the beginning.</p>
+            </dive>
             
-            <div className='container mx-auto w-1/4 h-1/4'>
+            <div className='flex mx-auto w-1/4 h-1/4 pt-5'>
                 <img src={Me} className='object-scale-down' style={{ width: "100%" }} alt="cover" />
+            </div>
+
+            <div className='flex-wrap pt-5'>
+                <p>Welcome to my portfolio.</p>
+                <br></br>
+                <p>I'm a full-stack web developer with a background in education. My love of learning has brought me to programming, and I hope that someday I can share that passion with many more!</p>
             </div>
             
         </div>
