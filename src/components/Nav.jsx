f17b1d8e8ade359d6a2604331031efdd07057de9
unknown <steinway.nathan@gmail.com>
2022-04-10 21:03:31 -0500

    put together basic navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 function Nav() {
     return(
         <header>
-            NAV
+            <nav>
+                <ul className='fixed w-full h-[100px] flex justify-evenly items-center px-4 bg-[#F9F5F5] text-white-300'>
+
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Work</li>
+                    <li>Skills</li>
+                    <li>Contact</li>
+
+                </ul>
+            </nav>
         </header>
     )
 }
