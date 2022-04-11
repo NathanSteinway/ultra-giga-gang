3f9fb996a59c60295891a6df4b7ce3123ff0edcc
unknown <steinway.nathan@gmail.com>
2022-04-11 18:11:00 -0500

    added 12 skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import HTML from '../assets/Skills Images/HTML5_Badge_256.png';
 import CSS from '../assets/Skills Images/css.png';
+import Git from '../assets/Skills Images/github.png';
+import Java from '../assets/Skills Images/javascript.png';
+import Mongo from '../assets/Skills Images/mongo.png';
+import Node from '../assets/Skills Images/node.png';
+import ReactBadge from '../assets/Skills Images/react.png';
+import Tailwind from '../assets/Skills Images/tailwind.png';
+import MySQL from '../assets/Skills Images/mysql.png';
+import Bootstrap from '../assets/Skills Images/bootstrap.png';
+import Mongoose from '../assets/Skills Images/mongoose.png';
+import Sequelize from '../assets/Skills Images/sequelize.png';
 
 function Skills() {
     return(
@@ -8,31 +18,49 @@ function Skills() {
         <div className='w-full h-screen bg-[#261A2B] text-white-300 py-4'>
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div>
-                    <p className='text-4xl inline border-b-4 border-white-300'>Skills</p>
+                    <p className='text-4xl mx-auto inline border-b-4 border-white-300'>Skills</p>
                 </div>
                 {/* Container for Skills images*/}
-                <div className='w-full grid grid-cols-4 gap-2 py-4 text-center'>
+                <div className='w-full grid grid-cols-3 gap-5 py-8 text-center'>
                     <div>
                         <img className='w-20 mx-auto' src={HTML} alt="HTML Logo" />
                     </div>
                     <div>
-                        <img className='w-20 mx-auto' src={CSS} alt="HTML Logo" />
+                        <img className='w-20 mx-auto' src={CSS} alt="CSS Logo" />
                     </div>
                     <div>
-                        <img className='w-20 mx-auto' src={HTML} alt="HTML Logo" />
+                        <img className='w-20 mx-auto' src={Git} alt="Git Logo" />
                     </div>
                     <div>
-                        <img className='w-20 mx-auto' src={HTML} alt="HTML Logo" /> 
+                        <img className='w-20 mx-auto' src={Java} alt="JavaScript Logo" /> 
                     </div>
                     <div>
-                        <img className='w-20 mx-auto' src={HTML} alt="HTML Logo" />   
+                        <img className='w-20 mx-auto' src={Mongo} alt="MongoDB Logo" />   
                     </div>
                     <div>
-                        <img className='w-20 mx-auto' src={HTML} alt="HTML Logo" />   
+                        <img className='w-20 mx-auto' src={Node} alt="Node.js Logo" />   
                     </div>
                     <div>
-                        <img className='w-20 mx-auto' src={HTML} alt="HTML Logo" />  
+                        <img className='w-20 mx-auto' src={ReactBadge} alt="React Logo" />  
                     </div>
+                    <div>
+                        <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind Logo" />  
+                    </div>
+                    <div>
+                        <img className='w-20 mx-auto' src={MySQL} alt="MySQL Logo" />  
+                    </div>
+                    <div>
+                        <img className='w-20 mx-auto' src={Bootstrap} alt="Bootstrap Logo" />  
+                    </div>
+                    <div>
+                        <img className='w-20 mx-auto' src={Mongoose} alt="Mongoose Logo" />  
+                    </div>
+                    <div>
+                        <img className='w-20 mx-auto' src={Sequelize} alt='Sequelize Logo' />
+                    </div>
+                </div>
+                <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+                    and more to come...
                 </div>
             </div>
 
