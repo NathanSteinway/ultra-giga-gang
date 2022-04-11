cc02f1b5b145cfc51efd28945e4f0a1d950c83a1
unknown <steinway.nathan@gmail.com>
2022-04-10 23:25:23 -0500

    fixed image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import BrotherK from '../assets/IMG_20180701_155555.jpg';
+import Me from '../assets/me.jpg';
 
 function About() {
 
     return(
 
-        <div className='max-w-md mx-auto px-8 flex flex-col justify-center h-full'>
+        <div className='mx-auto px-8 flex flex-col justify-center h-full'>
 
-            <div className='mx-auto'>
-                <h1>NATHAN STEINWAY</h1>
+            <div className='text-center'>
+                <h1 className='text-5xl'>NATHAN STEINWAY</h1>
             </div>
             
-            <div>
-                <img src={BrotherK} style={{ width: "100%" }} alt="cover" />
+            <div className='container mx-auto w-1/4 h-1/4'>
+                <img src={Me} className='object-scale-down' style={{ width: "100%" }} alt="cover" />
             </div>
             
         </div>
